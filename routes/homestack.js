69b2822619990a277038728aaa085ac1e9d20bb1
Emmanuel Akinyemi <58298846+emmanuel-akinyemi-Dev@users.noreply.github.com>
2024-01-07 23:22:39 +0100

    added dependencies

---
+++ routes/homestack.js
@@ -0,0 +1,13 @@
+import{createStackNavigator} from 'react-navigation-stack';
+import{createAppContainer} from 'react-navigator'
+
+const screens = {
+    Home:{
+        screen:{
+            
+        }
+    }
+}
+
+
+const HomeStack= createStackNavigator(screens)
